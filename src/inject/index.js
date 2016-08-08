a32a2b252ce64a9104ded3aa138f9d59418a4880
Justin Sisley <manmademammal@Justins-MacBook-Pro.local>
2016-08-08 15:49:06 -0700

    Added injection utility

---
+++ src/inject/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+// Take a compiled template and inject replacement values
+module.exports = (template, vars) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(
+      template,
+      { encoding: 'utf8' },
+      (err, data) => {
+        if (err) { return reject(err); }
+
+        var finalTemplate = data; // eslint-disable-line
+
+        Object.keys(vars).forEach(key => {
+          const regex = new RegExp(`{${key}}`, 'g');
+          finalTemplate = finalTemplate.replace(regex, vars[key]);
+        });
+
+        return resolve(finalTemplate);
+      }
+    );
+  });
--- src/mjml-utils.js
+++ src/mjml-utils.js
@@ -4,7 +4,13 @@ const argv = require('yargs').argv;
 const buildAll = require('./builder').buildAll;
 const watcher = require('./watcher');
 const sender = require('./sender');
+const inject = require('./inject');
 
 if (argv.build) { buildAll(argv.i, argv.o); }
 if (argv.watch) { watcher(argv.i, argv.o); }
 if (argv.send) { sender(argv.o); }
+
+// Non-cli utils
+module.exports = {
+  inject,
+};
